c06fa0eb681e23b4995ee19ca6d932df786bc181
Kamen MacKay <kamen.mackay@gmail.com>
2019-12-16 08:27:12 +0000

    Inital commit

---
+++ calculator/calculator.py
@@ -0,0 +1,9 @@
+"""
+Basic calculator in Python
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
